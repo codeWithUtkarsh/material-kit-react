b98cb7ffcb27cd506d74515f48da670411e0149e
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2023-11-28 11:25:37 +0000

    additional changes
    Signed-off-by: Utkarsh Kumar Sharma <utkarshkviim@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,47 +17,58 @@ const Page = () => {
     // Create a new JSZip instance
     const zip = new JSZip();
 
-    // Function to iterate through the blobs in the container and add them to the zip
     const addBlobsToZip = async (d) => {
       // Initialize Azure Storage Blob Service Client
       console.log('Form submitted with data:', d);
-      
+    
       const blobServiceClient = BlobServiceClient.fromConnectionString('BlobEndpoint=https://utkarshdemorg8b53.blob.core.windows.net/;QueueEndpoint=https://utkarshdemorg8b53.queue.core.windows.net/;FileEndpoint=https://utkarshdemorg8b53.file.core.windows.net/;TableEndpoint=https://utkarshdemorg8b53.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiyx&se=2025-11-26T00:55:50Z&st=2023-11-25T16:55:50Z&spr=https,http&sig=zSAsd2SMD86z3a9EPpvMa%2Bxhe9%2B828nLhkhlD3h5iw0%3D');
       const containerClient = blobServiceClient.getContainerClient('demo-project');
       const parent = d['language']
       const child = d['description']
-
+    
+      const downloadPromises = [];
+    
       for await (const blob of containerClient.listBlobsFlat()) {
-        const blobArr = blob.name.split('/')
-        
+        const blobArr = blob.name.split('/');
+    
         console.log('Form submitted with data:', blobArr[0] == parent && child.includes(blobArr[1]));
-        if(blobArr[0] == parent && child.includes(blobArr[1])){
+        if (blobArr[0] == parent && child.includes(blobArr[1])) {
           const blobClient = containerClient.getBlobClient(blob.name);
-          const blobContent = await blobClient.downloadToBuffer();
-          zip.file(blob.name, blobContent);
+          // const downloadPromise = blobClient.downloadToBuffer().then((blobContent) => {
+          //   zip.file(blob.name, blobContent);
+          // });
+
+          const downloadPromise = blobClient.downloadToBuffer().then((blobContent) => {
+            console.log('Blob Content:', blobContent);
+            // zip.file(blob.name, blobContent);
+            zip.file(blob.name, blobContent, { binary: true});
+
+          });
+    
+          downloadPromises.push(downloadPromise);
         }
       }
+    
+      // Wait for all blob downloads to complete
+      await Promise.all(downloadPromises);
     };
 
     // Call the async function to add blobs to the zip
     await addBlobsToZip(data);
 
-    const blob = await zip.generateAsync({ type: 'blob' });
+    const generatedBlob = await zip.generateAsync({ type: 'blob' });
     
-    // Generate the zip file
-    zip.generateAsync({ type: 'blob' }).then((blob) => {
-      // Trigger download of the generated zip file
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'generated-project.zip';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
+    // Trigger download of the generated zip file
+    const url = window.URL.createObjectURL(generatedBlob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'generated-project.zip';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
 
-      setRefreshKey((prevKey) => prevKey + 1);
-    });
+    setRefreshKey((prevKey) => prevKey + 1);
     
   };
 
